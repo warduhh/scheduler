db67306b69b8053b56dffd472d07b190dc894a6f
Warda <warda@wardas-macbook.home>
2023-05-27 13:56:38 -0400

    changed props name to value,onchange

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,8 +3,9 @@ import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
-  const { interviewers, setInterviewer, interviewer } = props;
+  const { interviewers, value, onChange } = props;
 
+  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
