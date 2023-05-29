085a5da9842de20bedbabcfd9e61c541c4850d5e
Warda <warda@Wardas-MacBook.local>
2023-05-29 14:33:49 -0400

    Created Appointment components

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "components/Appointment/style.scss"
+
+
+export default function Appointment(props) {
+
+  return (
+    <article className="appointment"></article>
+  );
+};
+
+
