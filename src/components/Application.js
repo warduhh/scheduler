f5946d7774e63312814224cc4fe5f5c99c329903
Warda <warda@Wardas-MacBook.local>
2023-06-02 17:45:52 -0400

    Used the useState to add days and created an effect to make GET request using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 
 import "components/Appointment";
 import Appointment from "components/Appointment";
+import axios from "axios";
+
+
 
 const appointments = {
   "1": {
@@ -46,28 +49,20 @@ const appointments = {
 
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    // console.log("useEffect running");
+    const url = `http://localhost:8001/api/days`;
+    axios.get(url).then((response) => {
+      // console.log(response.data);
+      setDays(response.data);
+    });
+  }, []);
+
 
   return (
     <main className="layout">
